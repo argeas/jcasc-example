05f088ec4e53a47d41737233e29f8751a629cd18
Leonidas <leonidas.kane@gmail.com>
2019-10-14 01:22:21 +0100

    small fix

--- Jenkins/Jenkinsfile.groovy
+++ Jenkins/Jenkinsfile.groovy
@@ -7,7 +7,7 @@ pipeline {
                     
                     script {
                         
-                        sh"""pipenv run pytest -s test_characters_json_validator.py
+                        sh"""pipenc install && pipenv run pytest -s test_characters_json_validator.py
                         """
                         echo "Hello from JCASC WORLD"
                     
